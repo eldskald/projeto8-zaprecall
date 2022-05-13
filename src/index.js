f27a6803bfcc36a88a28f431cf3b4eb48edc14c1
rafael <rafaelbordoni00@gmail.com>
2022-05-13 04:05:29 -0300

    Add initial files and project organization

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import App from 'components/App/App';
+import './assets/reset.css';
+import './assets/general.css';
+
+ReactDOM.render(<App />, document.querySelector(".root"));
